53d51b60247ab74dd9f530906ce4b52f7c1904c0
rentianyu <1782898491@qq.com>
2020-03-29 13:41:22 +0800

    2020-03-29 13:41:22 add my ad hosts

---
+++ push.sh
@@ -0,0 +1,2 @@
+# 推送到GitHub
+git add /sdcard/ADM/Github/Ad-set-hosts/* && git commit -m " `date '+%Y-%m-%d %T'` add my ad hosts " && git push -u origin master && echo -e " `date '+%Y-%m-%d %T' ` 更新hosts成功"
