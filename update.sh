4ccdb6b0d8a7c8a71cc3245e747ad1a121eb24e8
rentianyu <1782898491@qq.com>
2020-03-29 12:59:36 +0800

    date '+%Y-%m-%d %T' 提交

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # pull云端到本地并删除本地缓存
-git pull origin master && rm -f /sdcard/*.tmp
 
 # 基础版2357
 
@@ -23,4 +22,4 @@ t=/sdcard/host.tmp && for i in https://raw.githubusercontent.com/vokins/yhosts/m
 t=/sdcard/host.tmp && for i in https://raw.githubusercontent.com/E7KMbb/AD-hosts/master/system/etc/hosts https://raw.githubusercontent.com/vokins/yhosts/master/hosts https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts https://hosts.nfz.moe/full/hosts https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/xiaobeita/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/NetEase/hosts ;do curl -s "$i";echo "# $i" >>$t.tmp;done>$t && ((500<`wc -l < $t`)) && (echo -e "# `date '+%Y-%m-%d %T'` \n# Script by WQY916\n# Made by xiaobeita\n# This file is generated from the following sources:";cat $t.tmp;echo -e "\n\n127.0.0.1 localhost\n::1 localhost\n\n";sed '/localhost/d;/#/d;/^$/d;/^f/d;s/\r//g;s/\t/ /g;s/0.0.0.0/127.0.0.1/' $t|sort -u;rm $t $t.tmp)>/sdcard/ADM/Github/Ad-set-hosts/full-music/hosts && echo "操作成功..."||echo "操作失败..."
 
 # 推送到GitHub
-git add /sdcard/ADM/Github/Ad-set-hosts/* && git commit -m "date '+%Y-%m-%d %T' 提交" && git push -u origin master && echo " `date '+%Y-%m-%d %T' `更新hosts成功"
+git add /sdcard/ADM/Github/Ad-set-hosts/* && git commit -m "date '+%Y-%m-%d %T' 提交" && git push -u origin master && echo " date '+%Y-%m-%d %T' 更新hosts成功"
