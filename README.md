1e0117aea854599961616121c079f21d305927c8
rentianyu <1782898491@qq.com>
2020-04-21 10:27:40 +0800

    del music

--- README.md
+++ README.md
@@ -2,18 +2,17 @@
 
 ## 1. 简介
 
-- 本项目集合多个去广告`hosts`并去重，并且添加安卓端网易云音乐特权听歌`hosts`
+- 本项目集合多个去广告`hosts`并去重
 - 仅供学习交流，请勿商用
 - 如有侵权，请联系我删除
 
 ## 2. 使用方法
 
 - 下载所需hosts来替换系统hosts，需要root权限
-
 - 安卓`hosts`文件位置：`/system/etc/hosts`
+- 电脑勿用，`hosts`文件过大会造成电脑卡顿
 
-无音乐版：https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/full/hosts   
-有音乐版：https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/full-music/hosts   
+hosts地址：https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/hosts   
 
 ## 3. 在此致谢以下引用的hosts源以及@WQY916提供的帮助
 
@@ -24,6 +23,3 @@
 6. neoHosts完整版：https://hosts.nfz.moe/full/hosts   
 7. 小贝塔维护hosts：https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/xiaobeita/hosts   
 8. StevenBlack：https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts   
-9. 网易云音乐hosts：https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/NetEase/hosts   
-
-注：8 为`https://wy.ydlrqx.com/proxy.pac `提取得来
--- update.sh
+++ update.sh
@@ -6,9 +6,8 @@ git pull origin master
 # 下载去广告hosts合并并去重
 
 t=/sdcard/ADM/Github/Ad-set-hosts/hosts.txt
-f=/sdcard/ADM/Github/Ad-set-hosts/full/hosts
-m=/sdcard/ADM/Github/Ad-set-hosts/NetEase/hosts
-fm=/sdcard/ADM/Github/Ad-set-hosts/full-music/hosts
+f=/sdcard/ADM/Github/Ad-set-hosts/hosts
+
 
 rm $t
 
@@ -20,20 +19,16 @@ https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts
 https://hosts.nfz.moe/127.0.0.1/full/hosts
 https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/xiaobeita/hosts
 https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
-https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/NetEase/hosts
 EOF
 
 # 保留必要hosts
 sed -i '/^\(127\|0\|::\)/!d;s/0.0.0.0/127.0.0.1/g;/ip6-/d;/localhost/d;s/#.*//g' $t
 
 # 更新无音乐版hosts
-(echo -e "# `date '+%Y-%m-%d %T'`\n# 小贝塔自用，请勿商用\n127.0.0.1 localhost\n::1 localhost\n\n" && sort -u $t) >$f&&echo "更新无音乐版成功"||echo "更新无音乐版失败..."
-
-# 更新有音乐版hosts
-cat $m $f>$fm&&echo "更新有音乐版成功"||echo "更新有音乐版失败..."
+(echo -e "# `date '+%Y-%m-%d %T'`\n# 小贝塔自用，请勿商用\n127.0.0.1 localhost\n::1 localhost\n\n" && sort -u $t) >$f&&echo "更新hosts成功"||echo "更新hosts失败..."
 
 # 复制hosts到手机系统
-su -c 'mount -o rw,remount /system&&cp -f /sdcard/ADM/Github/Ad-set-hosts/full-music/hosts /system/etc/hosts&&mount -o ro,remount /system&&rm /sdcard/ADM/Github/Ad-set-hosts/hosts.txt&&echo "操作成功..."||echo "操作失败..."'
+su -c 'mount -o rw,remount /system&&cp -f /sdcard/ADM/Github/Ad-set-hosts/hosts /system/etc/hosts&&mount -o ro,remount /system&&rm /sdcard/ADM/Github/Ad-set-hosts/hosts.txt&&echo "操作成功..."||echo "操作失败..."'
 
 # 推送到GitHub
 git add /sdcard/ADM/Github/Ad-set-hosts/* && git commit -m " `date '+%Y-%m-%d %T'` " && git push -u origin master && echo -e " `date '+%Y-%m-%d %T' ` 更新hosts成功"||echo "更新hosts失败..."
